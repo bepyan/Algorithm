ab1f268e88ad3eee0659b234b38d1661f5fd70b5
bepyan <65283190+bepyan@users.noreply.github.com>
2021-08-16 19:05:58 +0900

    21.08.16

---
+++ Dynamic Programming/양팔저울.cpp
@@ -0,0 +1,36 @@
+// 백준 골드 2
+// https://www.acmicpc.net/problem/2629
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int weight_count, req_count;
+int weights[31];
+bool dp[31][15001] = {};
+
+void getWeight(int idx, int weight){
+    if(idx > weight_count) return;
+    if(dp[idx][weight] > 0) return;
+    if(weight > weight_count * 500) return;
+
+    dp[idx][weight] = 1;
+
+    getWeight(idx + 1, weight);
+    getWeight(idx + 1, weight + weights[idx]);
+    getWeight(idx + 1, abs(weight - weights[idx]));
+}
+
+int main(){
+    cin >> weight_count;
+    for (int i = 0; i < weight_count; i++)
+        cin >> weights[i];
+
+    getWeight(0, 0);
+
+    cin >> req_count;
+    for (int n; req_count--; ){
+        cin >> n;
+        cout << (dp[weight_count][n] ? "Y " : "N ");
+    }
+}
